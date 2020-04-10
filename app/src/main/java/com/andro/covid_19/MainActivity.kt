3f2245fad8c0c4a734bb38344b681fdfc5fb8414
Asmaa <asmaatarek93@gmail.com>
2020-04-10 21:24:18 +0200

    add map button merging manal branch

--- app/src/main/java/com/andro/covid_19/MainActivity.kt
+++ app/src/main/java/com/andro/covid_19/MainActivity.kt
@@ -2,6 +2,7 @@ package com.andro.covid_19
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -75,14 +76,11 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)*/
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
 
    /* override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }*/
+
+
 }
--- app/src/main/java/com/andro/covid_19/ui/home/HomeFragment.kt
+++ app/src/main/java/com/andro/covid_19/ui/home/HomeFragment.kt
@@ -2,9 +2,7 @@ package com.andro.covid_19.ui.home
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -14,7 +12,6 @@ import com.andro.covid_19.data.api_services.ApiHandler
 import com.andro.covid_19.data.api_services.ApiInterface
 import com.andro.covid_19.data.network.ConnectivityInterceptorImpl
 import com.andro.retro.json_models.CountriesStat
-import com.andro.retro.json_models.StatByCountry
 import com.andro.retro.json_models.WorldTotalStates
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.runBlocking
@@ -22,48 +19,50 @@ import kotlinx.coroutines.runBlocking
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private lateinit var homeAdapter :HomeAdapter
-
+    private lateinit var homeAdapter: HomeAdapter
 
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+
     ): View? {
         HomeViewModel.context = this!!.context!!
 
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         save()
-        homeViewModel.getCountriesData().observe(viewLifecycleOwner, Observer<List<CountriesStat>> {this.renderCountries(it)
-            })
-       /* homeViewModel.getWorldTotalStates().observe(viewLifecycleOwner, Observer<List<WorldTotalStates>> {this.renderWorldTotalStates(it.last())
-        })*/
+        homeViewModel.getCountriesData().observe(viewLifecycleOwner, Observer<List<CountriesStat>> {
+            this.renderCountries(it)
+        })
+        /* homeViewModel.getWorldTotalStates().observe(viewLifecycleOwner, Observer<List<WorldTotalStates>> {this.renderWorldTotalStates(it.last())
+         })*/
+        setHasOptionsMenu(true)
 
-            return root
+        return root
 
 
     }
 
-    private fun renderCountries(countries:List< CountriesStat>)
-    {
+    private fun renderCountries(countries: List<CountriesStat>) {
         progress_bar.setVisibility(View.GONE)
-        homeAdapter= HomeAdapter(countries)
+        homeAdapter = HomeAdapter(countries)
         val layoutManger = LinearLayoutManager(getActivity())
         //layoutManger.stackFromEnd = true
         allCounties_recyclerview.layoutManager = layoutManger
         allCounties_recyclerview.adapter = homeAdapter
     }
-    private fun renderWorldTotalStates(worldTotalStates:WorldTotalStates)
-    {
+
+    private fun renderWorldTotalStates(worldTotalStates: WorldTotalStates) {
         Log.d("myTag", ":WorldTotalStates")
         tv_infected.text = worldTotalStates.total_cases
         tv_death.text = worldTotalStates.total_deaths
         tv_recovered.text = worldTotalStates.total_recovered
     }
-    private fun save()
-    {
+
+    private fun save() {
         val api = ApiInterface(ConnectivityInterceptorImpl(context!!))
         val apiHandler = ApiHandler(api)
         runBlocking {
@@ -80,12 +79,33 @@ class HomeFragment : Fragment() {
         })
         apiHandler.worldState.observe(viewLifecycleOwner, Observer {
             Log.d("myTag", it.toString())
-                homeViewModel.setWorldTotalStates(it)
+            homeViewModel.setWorldTotalStates(it)
+            homeViewModel.getWorldTotalStates()
+                .observe(viewLifecycleOwner, Observer<List<WorldTotalStates>> {
+                    this.renderWorldTotalStates(it.last())
+                })
 
         })
-        homeViewModel.getWorldTotalStates().observe(viewLifecycleOwner, Observer<List<WorldTotalStates>> {this.renderWorldTotalStates(it.last())
-        })
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.map_button, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.mapButton) {
+//            if (trips != null && !trips.isEmpty()) {
+//
+//            }
+//            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 
 }
