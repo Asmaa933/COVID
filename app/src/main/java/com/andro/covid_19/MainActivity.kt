244de64b0b01101bb0d4751b4bf4220b2ff48138
Asmaa <asmaatarek93@gmail.com>
2020-04-20 21:43:25 +0200

    solving master conflicts

--- app/src/main/java/com/andro/covid_19/MainActivity.kt
+++ app/src/main/java/com/andro/covid_19/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
 import androidx.navigation.ui.AppBarConfiguration
 import com.andro.covid_19.ui.history.HistoryFragment
 import com.andro.covid_19.ui.home.HomeFragment
